0e6971f4966e71b1bc23d7033a6fc281d390ebc6
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-03-06 20:24:28 +0530

    Create PQ3.py

---
+++ PQ3.py
@@ -0,0 +1,12 @@
+#delete all 3rd number from list
+
+def delete(data):
+    if len(data)>=3:
+        del data[2]
+        return data
+    else:
+        return data
+        
+lists=input("enter the numbers seperated by space: ")  
+data=lists.split()
+print(delete(data))
