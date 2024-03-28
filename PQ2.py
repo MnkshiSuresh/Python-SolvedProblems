f86d5bc852418a8c6e47e59f41f172ce58eb1e4e
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-03-28 10:35:07 +0530

    Update PQ2.py

--- PQ2.py
+++ PQ2.py
@@ -1,4 +1,4 @@
-                       # Write a Python program to calculate the difference between a given number and 17. If the number is greater than 17, return twice the absolute difference.
+# Write a Python program to calculate the difference between a given number and 17. If the number is greater than 17, return twice the absolute difference.
 
 n= int(input("enter the number"))
 diff=n-17
