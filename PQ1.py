0f76a3552a252d97cd238497ae0f76406206d7ad
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-04-21 19:48:38 +0530

    Update PQ1.py

--- PQ1.py
+++ PQ1.py
@@ -1,6 +1,6 @@
     # Write a Python function that takes a positive integer and returns the sum of the cube of all positive integers smaller than the specified number.       
      
-def cubing(data):
+def cubing(data):  
     sum = 0
     for i in range(1,data+1):
         sum = sum + i**3  
