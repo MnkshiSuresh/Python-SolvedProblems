183bee880b569c65c4f658588f355471059521a9
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-04-21 17:45:32 +0530

    Create PQ8.py

---
+++ PQ8.py
@@ -0,0 +1,9 @@
+#Check whether variable is integer or string=====ISINSTANCE
+
+
+print(isinstance("25",str))
+print(isinstance("23",int))
+print(isinstance(25,str))
+print(isinstance(23,int))
+print(isinstance([25],list))
+print(isinstance([23],int))
